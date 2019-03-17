979ad7b42df37c05b324574d6c90440f266d2f00
Gusein Gadzhiev <gkgadzhiev@gmail.com>
2019-03-17 21:09:04 +0100

    Tool initial commit

---
+++ lib/gadzhiev_view_tool.rb
@@ -0,0 +1,6 @@
+require "gadzhiev_view_tool/version"
+require "gadzhiev_view_tool/renderer"
+
+module GadzhievViewTool
+  # Your code goes here...
+end
---
+++ lib/gadzhiev_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GadzhievViewTool
+  VERSION = "0.1.0"
+end
